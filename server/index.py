79d17dbfcfcb3e42bd89a44681a7caaf59552fd0
Michael Le <michael2017le@gmail.com>
2019-02-24 03:40:08 -0600

    MONGO WORKS FINALLY

--- server/index.py
+++ server/index.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
+mongo = PyMongo(app)
 
 @app.route("/")
 def index():
+  user = mongo.db.users.find({"x":2})
+  print(user[0])
   return render_template("index.html")
 
 @app.route("/hello")
